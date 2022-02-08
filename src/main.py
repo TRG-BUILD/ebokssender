7aba59d6751e733b5a930d2ff04473b3bcd7cc92
pellegoeg <pellegoeg@users.noreply.github.com>
2022-02-08 19:38:58 +0100

    Timing of files added to program

--- src/main.py
+++ src/main.py
@@ -4,40 +4,47 @@ Script to copy a single file over, waiting for a new file to occur and then star
 
 """
 from pathlib import Path
-from time import sleep
+from time import sleep, perf_counter_ns
 
 import shutil
 
+
 def main(work_dir: Path, file_list: list[Path]):
-    asking : str = ""
+    asking: str = ""
+    time_spend: list = [perf_counter_ns()]
     for file in file_list:
         print(f"Copy file {file} to {work_dir}")
 
-        if asking != 'a':
+        if asking != "a":
             asking = input("Go On [y/n/a]")
 
         if asking == "n":
             continue
 
-        org_file = file
-        work_file = work_dir / org_file.name
+        work_file = work_dir / file.name
 
         waiting_for_file = work_dir / f"{file.stem}.txt"
 
         if waiting_for_file.exists():
-            print(f"Skipping {org_file}...")
+            print(f"Skipping {file}...")
             continue
 
-        shutil.copy(org_file, work_file)
+        shutil.copy(file, work_file)
 
         print(f"Waiting for file: {waiting_for_file} to be computed... ")
 
         while not waiting_for_file.exists():
             sleep(0.3)
 
-        print(f"Finished {org_file} ")
+        print(
+            f"Finished {file} in {(time_spend[-1]-perf_counter_ns())/1000000000} seconds"
+        )
+        time_spend.append(perf_counter_ns())
     else:
         print(f"Finished working on all {len(file_list)}.")
+        print(f"Time used { (time_spend[-1]-time_spend[0])/1000000000 } seconds")
+        print()
+        print(time_spend)
 
 
 if __name__ == "__main__":
@@ -45,6 +52,6 @@ if __name__ == "__main__":
 
     work_dir = Path("../test/work")
 
-    file_list : list = sorted([file for file in data_dir.glob("*.pdf")])
+    file_list: list = sorted([file for file in data_dir.glob("*.pdf")])
 
-    main(data_dir, work_dir, file_list)
+    main(work_dir, file_list)
