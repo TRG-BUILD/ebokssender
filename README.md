2be1c565803bd7d5ca67c609603f1063215eb5ae
pellegoeg <pellegoeg@users.noreply.github.com>
2022-08-12 12:05:16 +0200

    README updated with example of compiling to .exe and program layout changed and code testet on mac os x

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ebokssender
 Program for sending large amounts of mails in digitalpost
+
+## Usage
+
+Run main.py at enter directory for pdf til send and the maxihotfolder directory
+
+
+## Compile to exe on windows
+
+```bash
+$ pip install pyinstaller
+```
+
+```bash
+$ pyinstaller --onefile src/main.py
+```
+
+Look for main.exe in /dist folder
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+
 """
 
 Script to copy a single file over, waiting for a new file to occur and then start with the next file.
@@ -12,9 +13,15 @@ import shutil
 def main(work_dir: Path, file_list: list[Path]):
     asking: str = ""
     time_spend: list = [perf_counter_ns()]
+    print("Files to process:")
+    print("-"*40)
+    print("\n".join([file.name for file in file_list]))
+    print()
+    print("Proces:")
+    print("-"*40)
     for idx, file in enumerate(file_list):
-        print()
-        print(f"[{(idx+1/len(file_list))}]Copy file {file} to {work_dir}")
+
+        print(f"[{idx+1}/{len(file_list)}] Copy file {file} to {work_dir}")
 
         if asking != "a":
             asking = input("Go On [y/n/a]")
@@ -24,9 +31,9 @@ def main(work_dir: Path, file_list: list[Path]):
 
         work_file = work_dir / file.name
 
-        waiting_for_file = work_dir / f"{file.stem}*.txt"
-
-        if waiting_for_file.exists():
+        waiting_for_file = f"{file.stem}*.log"
+        print(list(work_dir.glob(waiting_for_file)))
+        if any(waiting_for_file[:-5] in s.stem for s in list(work_dir.glob(waiting_for_file))):
             print(f"Skipping {file}...")
             continue
 
@@ -34,8 +41,7 @@ def main(work_dir: Path, file_list: list[Path]):
 
         print(f"Waiting for file: {waiting_for_file} to be computed... ")
 
-        while not waiting_for_file.exists():
-
+        while not any(waiting_for_file[:-5] in s.stem for s in list(work_dir.glob(waiting_for_file))):
             sleep(0.3)
 
         print(
@@ -46,14 +52,35 @@ def main(work_dir: Path, file_list: list[Path]):
         print(f"Finished working on all {len(file_list)}.")
         print(f"Time used { (time_spend[-1]-time_spend[0])/1000000000 } seconds")
         print()
-        print(time_spend)
+        while True:
+            close = input('Press Y to exit.')
+            if close.upper() == 'Y':
+                exit('Leaving Ebokssender')
+
+
+def read_input(ask: str) -> Path:
+    """
+    Typical directories is:
+    \\\\civil.aau.dk\\Fileshares\\ease\\Udsendelse
+    "c:\\Strålfors Files\\MaxiHotfolder\"
+    """
+
+    while True:
+        read = input(f"{ask} > ")
+        directory = Path(read)
+        if directory.exists():
+            return directory
+        else:
+            print("Directory dosent exists")
 
 
 if __name__ == "__main__":
-    data_dir = Path("../test/data")
+    print(read_input.__doc__)
 
-    work_dir = Path("../test/work")
+    data_dir = read_input("Path for pdf's to send")
 
-    file_list: list = sorted([file for file in data_dir.glob("*.pdf")])
+    working_directory = read_input("Working directory (Maxihotfolder)")
 
-    main(work_dir, file_list)
+    list_of_files: list = sorted([file for file in data_dir.glob("*.pdf")])
+
+    main(working_directory, list_of_files)
